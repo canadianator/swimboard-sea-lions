d7da5187c90473b01b3ec3e087dcc4727887a594
canadianator <zorozco@ztechaerial.com>
2026-06-03 11:13:26 -0500

    Update index.tsx

--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -7,24 +7,26 @@ import background from '../../images/sealionlogo.png';
 
 const CounterDiv = styled('div')`
   height: 75vh;
-  width: 70vw;
-  border-radius: 17vh;
+  width: 42vw; /* Adjusted width so both cards fit side-by-side comfortably within 100vw */
+  border-radius: 8vh; /* Slightly adjusted for better visual proportions */
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  box-shadow: 0px 10px 30px rgba(0,0,0,0.15); /* Soft shadow for better depth */
 `
 
 const CounterNumber = styled('div')`
   color: white;
-  font-size: 50vh;
+  font-size: 35vh; /* Brought down from 50vh to ensure 2-3 digit numbers don't overflow */
   width: 100%;
   text-align: center;
+  font-weight: bold;
 `
 
 const CounterLabel = styled('div')`
   color: white;
-  font-size: 5.5vw;
+  font-size: 4.5vw;
   width: 100%;
   text-align: center;
 `
@@ -35,6 +37,7 @@ const IndexPage: React.FC<PageProps> = () => {
     bullpen: 0,
     raceNumber: 0
   })
+
   React.useEffect(() => {
     if (controllerCounter) {
       setCounts(controllerCounter.getAllCounts());
@@ -54,27 +57,31 @@ const IndexPage: React.FC<PageProps> = () => {
       }
     }
   }, [controllerCounter]);
+
   return (
     <>
       <div style={{
-        width: '100%',
+        width: '100vw',
         height: '100vh',
         backgroundImage: `url(${background})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '40% auto', /* Places logo beautifully in the background center */
+        backgroundColor: '#f4f6f9', /* Fallback light background */
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'space-around'
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
       }}>
-        <CounterDiv id="left-counter" className='counter' style={{
-          backgroundColor: 'var(--counter-one)'
-        }}>
+        
+        {/* Left Board: Bullpen */}
+        <CounterDiv id="left-counter" className='counter'>
           <CounterNumber>{FormatCount(counts.bullpen)}</CounterNumber>
           <CounterLabel><b>Bullpen up to</b></CounterLabel>
         </CounterDiv>
-        <CounterDiv id="left-counter" className='counter' style={{
-          backgroundColor: 'var(--counter-two)'
-        }}>
+
+        {/* Right Board: Event/Race Number */}
+        <CounterDiv id="right-counter" className='counter'>
           <CounterNumber>{FormatCount(counts.raceNumber)}</CounterNumber>
           <CounterLabel><b>Event</b></CounterLabel>
         </CounterDiv>
